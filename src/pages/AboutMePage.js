1c5d149729026381856199507054811762c05ab2
Bravetomato <princeps2nd@gmail.com>
2023-02-20 23:05:49 +0900

    mainpage-button

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -3,11 +3,13 @@
 export default function AboutMePage() {
   const Header = () => {
     return (
-      <Header>
-        <h2>This is Header</h2>
-      </Header>
+      <div className="flex flex-col w-full">
+        <div className="grid h-20 card bg-base-300 rounded-box place-items-center">content</div> 
+        <div className="divider"></div> 
+        <div className="grid h-20 card bg-base-300 rounded-box place-items-center">content</div>
+      </div>
     );
    }
-  }
+ }
 
 
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,16 +1,23 @@
+import { fontSize, textAlign } from "@mui/system";
+import { Button } from "@mui/material";
 
 
 
 export default function MainPage() {
-  const style = {
-    display: "inline-block",
+  const mainStyle = {
     color: "#E26559",
+    fontSize: "61px",
+    textAlign: "center",
+  }
+  const nextButtonStyle = {
+    color: "#2B7799",
+    fontSize: "35px",
+  
   }
     return (
       <>
-      <div style={style} >
-        <h2 type="text" className="!p-20">WELLCOME TO MY PORTFOLIO</h2> 
-      </div>
+        <h2 type="text" style={mainStyle} className="p-20">WELLCOME TO MY PORTFOLIO</h2> 
+        <button style={nextButtonStyle} className="large p-20 ">START</button>
       </>
     );
    }
