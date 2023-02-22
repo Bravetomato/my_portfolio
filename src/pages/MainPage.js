57a5beeb4d0f7e7afcb117a691207facb073a7e4
Bravetomato <princeps2nd@gmail.com>
2023-02-22 23:53:22 +0900

    try2-Add Framer-motion

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,8 +1,19 @@
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
-import styled, { keyframes } from "styled-components"
+import { motion } from "framer-motion";
 
 export default function MainPage() {
+  const bgGradientStyle = {
+    width: "100wh",
+    height: "90vh",
+    color: "#fff",
+    // background: {linear-gradient(to right, #3d7eaa, #ffe47a)};
+    backgroundsize: "400% 400%",
+    // -webkit-animation: Gradient 15s ease infinite;
+    // -moz-animation: Gradient 15s ease infinite;
+    // animation: Gradient 15s ease infinite;
+  }
+
   const mainStyle = {
     color: "#E26559",
     fontSize: "61px",
@@ -16,6 +27,7 @@ export default function MainPage() {
   }
     return (
       <>
+       <motion.div whileDrag={{ backgroundColor:"#3d7eaa, #ffe47a" }} style={{bgGradientStyle}}/>
         <h2 type="text" style={mainStyle} className="p-20">WELLCOME TO MY PORTFOLIO</h2> 
         <Link to="/aboutme">
         <Button variant="outlined" style={nextButtonStyle} className="p-20 ">START</Button>
