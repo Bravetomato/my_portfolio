bf5b61a2d52af8e48160636633b910f5cde1e126
Bravetomato <princeps2nd@gmail.com>
2023-02-26 15:42:21 +0900

    aboutpage-resumebutton

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -1,9 +1,12 @@
 
 export default function AboutMePage() {
+    const openNewPage = (url) => {
+      window.open(url, "_blank", "noopener, noreferrer");
+    };
 
     return (
-      <div className="textarea" style={{backgroundColor: "#C5D7F2"}}>
-        <h2 className="aboutMeTitle" style={{fontSize: "40px"}}>About Me</h2>
+      <div className="textarea" style={{backgroundColor: "#C5D7F2", margin: "30px"}}>
+        <h2 className="aboutMeTitle" style={{fontSize: "40px", color: "#E26559"}}>About Me</h2>
         <h1>
         안녕하세요. 이곳은 제가 어떤 사람인지 적어 놓는 공간입니다.
         <br />
@@ -11,6 +14,7 @@ export default function AboutMePage() {
         <br />
         마지막에는 이력서 pdf를 연결하는 링크 버튼을 삽입할 계획입니다. 
         </h1>
+        <button onClick={() => openNewPage("")} style={{color:"#2B7799"}}>RESUME</button>
       </div>
     );
  }
