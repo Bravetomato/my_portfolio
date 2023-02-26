9bd7122439b137c26d3f58878327a06be944297e
Bravetomato <princeps2nd@gmail.com>
2023-02-26 16:25:57 +0900

    Add font- Noto Sans KR Medium 500

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -5,15 +5,15 @@ export default function AboutMePage() {
     };
 
     return (
-      <div className="textarea" style={{backgroundColor: "#C5D7F2", margin: "30px"}}>
+      <div className="textarea" style={{backgroundColor: "#84B9C0", margin: "30px"}}>
         <h2 className="aboutMeTitle" style={{fontSize: "40px", color: "#E26559"}}>About Me</h2>
-        <h1>
+        <article>
         안녕하세요. 이곳은 제가 어떤 사람인지 적어 놓는 공간입니다.
         <br />
         현재까지 경력과 개발을 시작한 계기, 가치관, 저의 장점을 적고,
         <br />
         마지막에는 이력서 pdf를 연결하는 링크 버튼을 삽입할 계획입니다. 
-        </h1>
+        </article>
         <button onClick={() => openNewPage("")} style={{color:"#2B7799"}}>RESUME</button>
       </div>
     );
