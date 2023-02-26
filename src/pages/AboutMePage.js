a5e8ab1ae230d9bb9a43624debf620e8d416cb13
Bravetomato <princeps2nd@gmail.com>
2023-02-26 15:08:59 +0900

    aboutpage-textbox -2

--- src/pages/AboutMePage.js
+++ src/pages/AboutMePage.js
@@ -2,10 +2,15 @@
 export default function AboutMePage() {
 
     return (
-      <div className="flex flex-col w-full bg-[#C5D72]">
-        <ul className="textBox grid h-20 bg-[#C5D72] card rounded-box place-items-center">
-          안녕하세요. 
-        </ul>
+      <div className="textarea" style={{backgroundColor: "#C5D7F2"}}>
+        <h2 className="aboutMeTitle" style={{fontSize: "40px"}}>About Me</h2>
+        <h1>
+        안녕하세요. 이곳은 제가 어떤 사람인지 적어 놓는 공간입니다.
+        <br />
+        현재까지 경력과 개발을 시작한 계기, 가치관, 저의 장점을 적고,
+        <br />
+        마지막에는 이력서 pdf를 연결하는 링크 버튼을 삽입할 계획입니다. 
+        </h1>
       </div>
     );
  }
