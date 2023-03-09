ad8e36e545929d96eda57868d0760e900090ad5b
Bravetomato <princeps2nd@gmail.com>
2023-03-09 20:06:01 +0900

    project img 작업-1

--- src/pages/ProjectsPage.js
+++ src/pages/ProjectsPage.js
@@ -22,7 +22,7 @@ export default function ProjectsPage() {
            <div className="flex relative">
             <img className="absolute inset-0 w-full h-full object-cover object-center" src={project.image}/> 
             <div className="px-8 py-10 relative z-10 w-full bg-gray-300 opacity-0 hover:opacity-100">
-              <h2 className="tracking-widest text-m titlefont tont-medium text-green-400 mb-1">{project.skills}</h2>
+              <h2 className="tracking-widest text-m titlefont font-medium text-green-400 mb-1">{project.skills}</h2>
               <h1 className="title-font text-lg font-medium text-white mb-3">{project.title}</h1>
               <span className="leading-relaxed">{project.description}</span>
             </div>
